f002ca10fc13b4acf0fa5380a053dae755f513b0
AnkeatOpenWindow <221202@virtualwindow.co.za>
2024-05-18 11:50:03 +0200

    Linked firebase with my project.
    - I created a firebase project in firebase and linked it up with my project.
    
    - I created a firebase.js file where I linked my project with my project.
    
    - On firebase I already implemented authentication using email and password.

---
+++ firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth"; //sets up auth functonality
+// TODO: Add SDKs for Firebase products that you want to use - Authentication, Firesotre & Storage 
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDaX-NbZSpEEhbdbMcfjipXamT9SXKt0ko",
+    authDomain: "enchantedreviews-61686.firebaseapp.com",
+    projectId: "enchantedreviews-61686",
+    storageBucket: "enchantedreviews-61686.appspot.com",
+    messagingSenderId: "980378383687",
+    appId: "1:980378383687:web:8dcc7f2f98cb3be2008862"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app); // Exporting our auth capibilities
